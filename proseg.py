f7b055fdfaa9470fa23b6a9ad06a95f3b3c2d334
chapa <167942843+redirigiendome@users.noreply.github.com>
2024-08-09 21:41:29 -0400

    Add files via upload
    PG2 / Area f G.F.

---
+++ proseg.py
@@ -0,0 +1,37 @@
+#Calcular el area de al menos 3 figuras geometricas
+#Menu para elegir la figura
+que="s"
+while que=="s":
+    print("Bienvenido al menú seleccionar una figura")
+    print("Seleccione cualquiera de estas 3 figuras")
+    print( )
+    print("1. Cuadrado ")
+    print("2. Triangulo")
+    print("3. Rectangulo")
+    print("4. Circulo")
+    selec=int(input("-> "))
+    if selec==1:
+        #Area de un cuadrado = Lado x Lado
+        L1=int(input("Introduzca un lado del cuadrado -> "))
+        Rep=L1*L1
+        print("El area del cuadrado es de: ",Rep)
+    if selec==2:
+        #Area de un triangulo = (Base x Altura) / 2
+        b=int(input("Introduzca la base del triangulo -> "))
+        h=int(input("Introduzca la altura del triangulo -> "))
+        rep2=(b*h)/2
+        print("El area del triangulo con las medidas introducidas es de: ",rep2)
+    if selec==3:
+        #Area de un rectangulo = Base x Altura
+        b=int(input("Introduzca la base del rectangulo -> "))
+        h=(int(input("Introduzca la altura del rectangulo ->")))
+        rep3=b*h
+        print("El area del rectangulo es de: ",rep3)
+    if selec==4:
+        #Area de un circulo = pi x Radio^2
+        r=int(input("Introduzca el radio del circulo -> "))
+        rep4=3.14*(r^2)
+        print("El area del circulo es de: ",rep4)
+    print("¿Quiere seguir usando el programa?")
+    print(" S / N ")
+    que=input("-> ")
